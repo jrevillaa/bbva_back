20fae3fb9bffb566b5f9ae6883a572ad3d2eb355
Jair Revilla <jrevilla492@gmail.com>
2019-07-19 13:18:06 -0500

    update

--- pagorecibo/src/main/java/com/bbva/pagorecibo/controllers/RecieptController.java
+++ pagorecibo/src/main/java/com/bbva/pagorecibo/controllers/RecieptController.java
@@ -2,16 +2,22 @@ package com.bbva.pagorecibo.controllers;
 
 import com.bbva.pagorecibo.entity.Receipt;
 import com.bbva.pagorecibo.payload.UserConsult;
+import com.bbva.pagorecibo.repository.ReceiptRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class RecieptController {
     
+    @Autowired
+	private ReceiptRepository receiptRepository;
+    
     @GetMapping(name = "/api/dashboard")
-	public Receipt dashboard(@RequestBody UserConsult userConsult) {
+	public List<Receipt> dashboard(@RequestBody UserConsult userConsult) {
 		
-            userConsult.getId();
-                    
-            return null;
+           //return ReceiptRepository.findByUserId(userConsult.getId());
+           return receiptRepository.findByUserId(userConsult.getId());
+
 	}
 }
--- pagorecibo/src/main/java/com/bbva/pagorecibo/repository/ReceiptRepository.java
+++ pagorecibo/src/main/java/com/bbva/pagorecibo/repository/ReceiptRepository.java
@@ -6,11 +6,15 @@
 package com.bbva.pagorecibo.repository;
 
 import com.bbva.pagorecibo.entity.Receipt;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
     
+    @Query("SELECT r FROM Receipt r WHERE r.user_id = ?1")
+	public List<Receipt> findByUserId(Long id);
 }
