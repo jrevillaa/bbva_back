b04d8f9dd235a0dce65a14696a94048fafef7122
unknown <villanuevahlm@gmail.com>
2019-07-19 10:22:11 -0500

    feat(pagorecibo): Se crea el proyecto base

---
+++ pagorecibo/src/main/java/com/bbva/pagorecibo/PagoreciboApplication.java
@@ -0,0 +1,13 @@
+package com.bbva.pagorecibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoreciboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoreciboApplication.class, args);
+	}
+
+}
---
+++ pagorecibo/src/test/java/com/bbva/pagorecibo/PagoreciboApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bbva.pagorecibo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PagoreciboApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
