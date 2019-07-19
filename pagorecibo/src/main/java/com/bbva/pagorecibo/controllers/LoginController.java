82538d9ee51c89a8a77d082da3ca02ab49da72ac
Jair Revilla <jrevilla492@gmail.com>
2019-07-19 12:42:53 -0500

    update

--- pagorecibo/src/main/java/com/bbva/pagorecibo/controllers/LoginController.java
+++ pagorecibo/src/main/java/com/bbva/pagorecibo/controllers/LoginController.java
@@ -1,19 +1,15 @@
 package com.bbva.pagorecibo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbva.pagorecibo.entity.User;
 import com.bbva.pagorecibo.payload.Usuario;
-import com.bbva.pagorecibo.service.LoginService;
 
 @RestController
 public class LoginController {
 
-	@Autowired
-	LoginService loginService;
 	
 	@PostMapping(name = "/api/login")
 	public User login(@RequestBody Usuario usuario) {
---
+++ pagorecibo/src/main/java/com/bbva/pagorecibo/payload/UserConsult.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bbva.pagorecibo.payload;
+
+/**
+ *
+ * @author jair
+ */
+public class UserConsult {
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
